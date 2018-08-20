96de8ed34e08c03e293adb9ec023768fec45418d
Ben.Woodhouse16 <ben.woodhouse@qa.com>
2018-08-20 17:17:13 +0100

    fixing year long n00b mistakes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM ubuntu:14.04
 
-WORKDIR  /
+RUN apt-get -y update && apt-get -y upgrade && apt-get install -y software-properties-common
+RUN add-apt-repository ppa:openjdk-r/ppa -y || exit 0 \
+&& apt-get update -y  &&apt-get install -y wget apache2 openjdk-8-jdk maven git
 
-ADD myScriptForJenkins.sh /
-RUN chmod +x myScriptForJenkins.sh
+RUN wget http://ftp-chi.osuosl.org/pub/jenkins/war-stable/2.121.3/jenkins.war
 EXPOSE 8080
 
-CMD = ["/myScriptForJenkins.sh"]
+CMD ["java", "-jar", "jenkins.war"]
--- myScriptForJenkins.sh
+++ myScriptForJenkins.sh
@@ -5,30 +5,4 @@
 
 
 
-#switich to root
-sudo -i
-
-apt-get update
-#curl and wget needed or jenkins installation
-apt-get install -y curl
-apt-get install -y wget
-
-
-#installing java
-add-apt-repository ppa:openjdk-r/ppa -y
-apt-get update
-apt-get -y install apache2 openjdk-8-jdk
-update-alternatives --config java 
-apt-get update
-
-#installing maven
-apt-get install -y maven
-apt-get update
-
-#installing Git
-apt-get install -y git
-
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
- sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
- apt-get update
-apt-get install jenkins
+jenkins start 
