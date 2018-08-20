67e3902f7880231cbe57fe652d9a0aa7b19239f8
Ben.Woodhouse16 <ben.woodhouse@qa.com>
2018-08-20 17:18:03 +0100

    cleanup

--- myScriptForJenkins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#script to setup working in java with maven on a ubuntu machine.
-#This sets up all the software the user will need to create a jenkins
-# environment that automates builds from Git
-
-
-
-jenkins start 
